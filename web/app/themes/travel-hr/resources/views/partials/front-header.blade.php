19748b400c159e5f5aa49b313c47101f36968f39
pandabrand <pandabrand@gmail.com>
2020-02-17 21:49:12 -0600

    update to nav

--- web/app/themes/travel-hr/resources/views/partials/front-header.blade.php
+++ web/app/themes/travel-hr/resources/views/partials/front-header.blade.php
@@ -1,10 +1,13 @@
 <header class="banner">
-  <div class="container-fluid">
+  <div class="container-fluid temp-padding">
+    <div class="hr-nav">
+      <a class="brand" href="https://www.hardrockhotels.com/"><img class="img-fluid" src="@asset('images/hard-rock-hotel-2-logo.png')"></a>
+      <a href="https://www.hardrockhotels.com/stay-like-a-legend-promotion" class="btn btn-primary inverse temp-line">Book Now</a>
+    </div>
     <nav class="nav-primary">
       @if (has_nav_menu('home_navigation'))
         {!! wp_nav_menu(['theme_location' => 'home_navigation', 'menu_class' => 'nav']) !!}
       @endif
     </nav>
-    <a class="brand" href="{{ home_url('/') }}"><img class="img-fluid" src="@asset('images/hard-rock-hotel-2-logo.png')"></a>
   </div>
 </header>
--- web/app/themes/travel-hr/resources/views/partials/header.blade.php
+++ web/app/themes/travel-hr/resources/views/partials/header.blade.php
@@ -1,6 +1,10 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="{{ home_url('/') }}"><img class="img-fluid" src="@asset('images/hard-rock-hotel-2-logo.png')"></a>
+  <div class="container temp-padding">
+    <div class="hr-nav">
+      <a class="brand" href="{{ home_url('/') }}"><img class="img-fluid" src="@asset('images/hard-rock-hotel-2-logo.png')"></a>
+      <a href="https://www.hardrockhotels.com/stay-like-a-legend-promotion" class="btn btn-primary inverse temp-line">Book Now</a>
+    </div>
+
     <nav class="nav-primary">
       @if (has_nav_menu('home_navigation'))
         {!! wp_nav_menu(['theme_location' => 'home_navigation', 'menu_class' => 'nav']) !!}
