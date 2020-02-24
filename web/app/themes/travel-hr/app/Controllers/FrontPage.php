d99933e0ce673802710598dd89d5c17c651ead4b
pandabrand <pandabrand@gmail.com>
2020-02-24 08:16:42 -0500

    shorten slice length

--- web/app/themes/travel-hr/app/Controllers/FrontPage.php
+++ web/app/themes/travel-hr/app/Controllers/FrontPage.php
@@ -37,7 +37,7 @@ class FrontPage extends Controller
                         'name' => $post->post_title,
                         'link' => get_the_permalink( $post->ID ),
                         'summary' => wp_trim_words( $post->post_content, '15', '' ),
-                        'locations' => array_slice( get_field( 'artists_locations', $post ), 0, 5 )
+                        'locations' => array_slice( get_field( 'artists_locations', $post ), 0, 4 )
                     );
                 }
             }
