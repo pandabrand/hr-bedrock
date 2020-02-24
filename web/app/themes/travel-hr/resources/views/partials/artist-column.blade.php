ab2c27031d237dafc85797761a715815b5a1da57
pandabrand <pandabrand@gmail.com>
2020-02-24 08:56:14 -0500

    header and front clean up

--- web/app/themes/travel-hr/resources/views/partials/artist-column.blade.php
+++ web/app/themes/travel-hr/resources/views/partials/artist-column.blade.php
@@ -1,9 +1,9 @@
 <div class="col-md-3 artist-column">
-  <a href="{{$link}}" rel="bookmark">{!! $image !!}</a>
-  <div class="title"><a href="{{$link}}" rel="bookmark">Meet {{$name}}</a></div>
-  <div class="summary">{{$summary}}</div>
-  <div><a href="{{$link}}" rel="bookmark">Learn More</a></div>
-  <div class="locations">
+  <a class="artist-child image" href="{{$link}}" rel="bookmark">{!! $image !!}</a>
+  <div class="title artist-child"><a href="{{$link}}" rel="bookmark">Meet {{$name}}</a></div>
+  <div class="summary artist-child">{{$summary}}</div>
+  <div class="artist-child link"><a href="{{$link}}" rel="bookmark">Learn More</a></div>
+  <div class="locations artist-child">
     <ul>
       @foreach( $locations as $location )
         <li>{{$location['location'][0]->post_title}}</li>
