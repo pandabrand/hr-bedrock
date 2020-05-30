2743791843d135b2f683d3d1d16faa0b7b34a520
pandabrand <pandabrand@gmail.com>
2020-05-30 17:11:23 -0500

    header feedback

--- web/app/themes/travel-hr/resources/views/partials/front-header.blade.php
+++ web/app/themes/travel-hr/resources/views/partials/front-header.blade.php
@@ -1,17 +1,16 @@
 <header class="banner">
-  <div class="container-fluid temp-padding">
-    <div class="hr-nav">
+  <div class="container">
+  <div class="d-flex justify-content-between align-items-center w-100">
       <a class="brand" href="https://www.hardrockhotels.com/"><img class="img-fluid" src="@asset('images/HRHotel_low-white.png')"></a>
-      <a href="https://www.hardrockhotels.com/stay-like-a-legend-promotion" class="btn btn-outline-light btn-lg btn-front-line">Book Now</a>
-    </div>
-    <nav class="nav-primary navbar-expand-md">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+      <nav class="nav-primary navbar-expand-md">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu-home-menu" aria-controls="menu-home-menu" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="fa fa-bars fa-2x"></span>
+        </button>
 
-      @if (has_nav_menu('home_navigation'))
-        {!! wp_nav_menu(['theme_location' => 'home_navigation', 'menu_class' => 'nav collapse navbar-collapse', 'id' => 'navbarToggler']) !!}
-      @endif
-    </nav>
+        @if (has_nav_menu('home_navigation'))
+          {!! wp_nav_menu(['theme_location' => 'home_navigation', 'menu_class' => 'nav collapse navbar-collapse', 'id' => 'navbarToggler']) !!}
+        @endif
+      </nav>
+    </div>
   </div>
 </header>
