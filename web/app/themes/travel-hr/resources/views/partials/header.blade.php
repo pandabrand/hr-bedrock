407d1a33a14df57cafd4d51564db2a6deab09916
pandabrand <pandabrand@gmail.com>
2020-06-05 13:38:19 -0500

    sync headers

--- web/app/themes/travel-hr/resources/views/partials/header.blade.php
+++ web/app/themes/travel-hr/resources/views/partials/header.blade.php
@@ -1,16 +1,16 @@
 <header class="banner">
   <div class="container">
-    <div class="d-flex justify-content-between align-items-center w-100">
-      <a class="brand" href="{{ home_url('/') }}"><img class="img-fluid" src="@asset('images/HRHotel_low-white.png')"></a>
+  <nav class="nav-primary navbar navbar-expand-md">
+      <a class="navbar-brand brand" href="https://www.hardrockhotels.com/"><img class="img-fluid" src="@asset('images/HRHotel_low-white.png')"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="fa fa-bars fa-2x"></span>
+      </button>
 
-      <nav class="nav-primary navbar navbar-expand-md navbar-dark">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu-home-menu" aria-controls="menu-home-menu" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="fa fa-bars fa-2x"></span>
-        </button>
+      <div id="navbarToggle" class="navbar-collapse collapse">
         @if (has_nav_menu('home_navigation'))
-          {!! wp_nav_menu(['theme_location' => 'home_navigation', 'container_class' => 'nav collapse navbar-collapse', 'id' => 'navbarToggler']) !!}
+        {!! wp_nav_menu(['theme_location' => 'home_navigation', 'menu_class' => 'navbar-nav', 'container_class' => 'nav-wrap ml-md-auto p-3 p-md-0']) !!}
         @endif
-      </nav>
-    </div>
+      </div>
+    </nav>
   </div>
 </header>
