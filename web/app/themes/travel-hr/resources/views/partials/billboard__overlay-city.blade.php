15b4514c899ca766dd49e833c62bab32529e76ee
pandabrand <pandabrand@gmail.com>
2020-06-14 14:35:04 -0500

    center billboard text

--- web/app/themes/travel-hr/resources/views/partials/billboard__overlay-city.blade.php
+++ web/app/themes/travel-hr/resources/views/partials/billboard__overlay-city.blade.php
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row no-gutters">
       <div class="col-12 d-flex align-items-center">
-        <div class="billboard__post-type-info">
+        <div class="billboard__post-type-info mx-auto">
           <div class="page-header">
             <h1 class="palmaton">Explore</h1>
             <h1>{!! get_the_title() !!}</h1>
