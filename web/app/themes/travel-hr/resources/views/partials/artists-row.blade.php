1b76251cedb891c382d6362eaa33db454aa19934
pandabrand <pandabrand@gmail.com>
2020-06-16 15:29:25 -0500

    update cta link

--- web/app/themes/travel-hr/resources/views/partials/artists-row.blade.php
+++ web/app/themes/travel-hr/resources/views/partials/artists-row.blade.php
@@ -7,7 +7,7 @@
             <h2 class="palmaton text-center">Choose</h2>
             <h2 class="text-center text-uppercase font-weight-bold">Your Music Concierge</h2>
           </div>
-          <a href="/city" class="btn btn-secondary text-uppercase mx-auto">See All Guides</a>
+          <a href="/artist" class="btn btn-secondary text-uppercase mx-auto">See All Guides</a>
         @elseif( 'city' == get_post_type() )
           <h2 class="palmaton text-center">Area Guides</h2>
           <h2 class="text-center text-uppercase font-weight-bold">Curated By Local Artists</h2>
