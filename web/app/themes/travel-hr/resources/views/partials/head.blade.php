703df26743daf6365e23ca6a7442c5ecaf37816b
pandabrand <pandabrand@gmail.com>
2020-06-16 19:04:51 -0500

    favicon and vibe mananger. seo

--- web/app/themes/travel-hr/resources/views/partials/head.blade.php
+++ web/app/themes/travel-hr/resources/views/partials/head.blade.php
@@ -2,5 +2,14 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="180x180" href="{!! \App\asset_path('images/favicon/apple-touch-icon.png') !!}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{!! \App\asset_path('images/favicon/favicon-32x32.png') !!}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{!! \App\asset_path('images/favicon/favicon-16x16.png') !!}">
+  <link rel="manifest" href="{!! \App\asset_path('images/favicon/site.webmanifest') !!}">
+  <link rel="mask-icon" href="{!! \App\asset_path('images/favicon/safari-pinned-tab.svg') !!}" color="#242c2e">
+  <link rel="shortcut icon" href="{!! \App\asset_path('images/favicon/favicon.ico') !!}">
+  <meta name="msapplication-TileColor" content="#2b5797">
+  <meta name="msapplication-config" content="{!! \App\asset_path('images/favicon/browserconfig.xml') !!}">
+  <meta name="theme-color" content="#000000">
   @php wp_head() @endphp
 </head>
